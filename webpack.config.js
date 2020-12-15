d6f688124f97459be1019e6abe35b61ed8be9815
Raed56 <rseraj@usf.edu>
2020-12-14 20:29:45 -0500

    The Road to Hello World: Creating first component

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
 module.exports = {
     entry: "./app/index.js",
     module: {
-        rules: [],
+        rules: [
+            { test: /\.svg$/, use: "svg-inline-loader" },
+            { test: /\.css$/, use: ["style-loader", "css-loader"] },
+            { test: /\.(js)$/, use: "babel-loader" },
+        ],
     },
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index_bundle.js",
+    },
+
+    plugins: [new HtmlWebpackPlugin()],
+
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
 };
