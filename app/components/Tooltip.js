bd22b561543f363dd8b00a3e15ab276dd1a0fff8
Raed56 <rseraj@usf.edu>
2021-01-11 17:53:19 -0500

    Code Sharing: Hover Render Prop

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Hover from "./Hover";
 
 const styles = {
     container: {
@@ -22,40 +23,17 @@ const styles = {
     },
 };
 
-export default class Tooltip extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            hovering: false,
-        };
-        this.mouseOver = this.mouseOver.bind(this);
-        this.mouseOut = this.mouseOut.bind(this);
-    }
-
-    mouseOver() {
-        this.setState({
-            hovering: true,
-        });
-    }
-    mouseOut() {
-        this.setState({
-            hovering: false,
-        });
-    }
-    render() {
-        const { text, children } = this.props;
-        const { hovering } = this.state;
-
-        return (
-            <div
-                onMouseOver={this.mouseOver}
-                onMouseOut={this.mouseOut}
-                style={styles.container}
-            >
-                {hovering === true && <div style={styles.tooltip}>{text}</div>}
-                {children}
-            </div>
-        );
-    }
+export default function Tooltip({ text, children, hovering }) {
+    return (
+        <Hover>
+            {(hovering) => (
+                <div style={styles.container}>
+                    {hovering === true && (
+                        <div style={styles.tooltip}>{text}</div>
+                    )}
+                    {children}
+                </div>
+            )}
+        </Hover>
+    );
 }
