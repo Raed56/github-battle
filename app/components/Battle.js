03bedd87d3369f5f1b7a877454868b43d51a5598
Raed56 <rseraj@usf.edu>
2021-01-13 20:50:57 -0500

    changed themes

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -8,38 +8,45 @@ import {
 } from "react-icons/fa";
 
 import Results from "./Results";
+import { ThemeConsumer } from "../contexts/theme";
 
 function Instructions() {
     return (
-        <div className="instructions-container">
-            <h1 className="center-text header-lg">Instructions</h1>
-            <ol className="container-sm grid center-text battle-instructions">
-                <li>
-                    <h3 className="header-sm">Enter two Github Users</h3>
-                    <FaUserFriends
-                        className="bg-light"
-                        color="rgb(255,191,116)"
-                        size={140}
-                    />
-                </li>
-                <li>
-                    <h3 className="header-sm">Battle</h3>
-                    <FaFighterJet
-                        className="bg-light"
-                        color="#727272"
-                        size={140}
-                    />
-                </li>
-                <li>
-                    <h3 className="header-sm">See the winners</h3>
-                    <FaTrophy
-                        className="bg-light"
-                        color="rgb(255,215,0)"
-                        size={140}
-                    />
-                </li>
-            </ol>
-        </div>
+        <ThemeConsumer>
+            {({ theme }) => (
+                <div className="instructions-container">
+                    <h1 className="center-text header-lg">Instructions</h1>
+                    <ol className="container-sm grid center-text battle-instructions">
+                        <li>
+                            <h3 className="header-sm">
+                                Enter two Github Users
+                            </h3>
+                            <FaUserFriends
+                                className={`bg-${theme}`}
+                                color="rgb(255,191,116)"
+                                size={140}
+                            />
+                        </li>
+                        <li>
+                            <h3 className="header-sm">Battle</h3>
+                            <FaFighterJet
+                                className={`bg-${theme}`}
+                                color="#727272"
+                                size={140}
+                            />
+                        </li>
+                        <li>
+                            <h3 className="header-sm">See the winners</h3>
+                            <FaTrophy
+                                className={`bg-${theme}`}
+                                color="rgb(255,215,0)"
+                                size={140}
+                            />
+                        </li>
+                    </ol>
+                </div>
+            )}
+        </ThemeConsumer>
     );
 }
 
