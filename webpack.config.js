cfda41512c6aadd9154b7de2b39f4cf6868fd311
Raed56 <rseraj@usf.edu>
2021-01-22 00:41:01 -0500

    Production Update

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         }),
     ],
 
-    mode: "development",
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
 
     devServer: {
         open: true,
